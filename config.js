027ea15134a3bd6dd7e6a91acd9ac2e8b4cd53fd
Andrey Pavlenko <pavlenko.andr98@yandex.ru>
2018-08-30 22:22:40 +0300

    add configuration file

---
+++ config.js
@@ -0,0 +1,33 @@
+/*
+ *
+ * Configs for the server
+ *
+ */
+
+// Container for all the environments
+var environments = {};
+
+// Debug configuration
+environments.debug = {
+  'envName': 'debug',
+  'httpPort': 8080,
+  'httpsPort': 8081
+};
+
+// Production configuration
+environments.production = {
+  'envName': 'production',
+  'httpPort': 80,
+  'httpsPort': 443
+};
+
+// Determine requested in command line environment
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ?
+        process.env.NODE_ENV.toLowerCase() : '';
+
+// Take chosen environment from the object or take default one
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ?
+        environments[currentEnvironment] : environments.debug;
+
+// Export the module
+module.exports = environmentToExport;
